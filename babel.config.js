96d9b17853720da83577e731fbbd6be0adc34c9b
patricpz <patricbarros19@gmail.com>
2025-12-26 09:24:27 -0300

    fix(error): error inicialization

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "allowlist": null,
-        "blocklist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
-    ]
   };
 };
